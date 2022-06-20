d544cf7331cb4a6180a1ae11224c6aaa34e7d47e
weixuntang <matth3w_wx@hotmail.com>
2022-06-21 03:05:33 +0800

    formatting updated and minor code refactor

--- minesweeper/src/main/java/self/tut/game/utilities/MineFieldControls.java
+++ minesweeper/src/main/java/self/tut/game/utilities/MineFieldControls.java
@@ -3,312 +3,313 @@ package self.tut.game.utilities;
 import java.util.Scanner;
 
 public class MineFieldControls {
-	
-	Scanner sc = new Scanner(System.in);
 
-	/**
-	 * Method to retrieve user input
-	 * 
-	 * @return user input in string.
-	 */
-	private String getUserInputInt(String input) {
-			System.out.printf(input);
+  Scanner sc = new Scanner(System.in);
 
-			return sc.nextLine().replaceAll("\\s+","");
-	}
+  /**
+   * Method to retrieve user input
+   * 
+   * @return user input in string.
+   */
+  private String getUserInputInt(String input) {
+    System.out.printf(input);
 
-	
-	/**
-	 * Method to set up the field for mine sweeper (visible)
-	 */
-	public void displayMineField(int[][] fieldVisible, int[][] fieldHidden) {
-
-		System.out.print("\t ");
-		for (int i = 0; i < 10; i++) {
-			System.out.print(" " + i + "  ");
-		}
-		System.out.print("\n");
-		for (int i = 0; i < 10; i++) {
-			System.out.print(i + "\t| ");
-			for (int j = 0; j < 10; j++) {
-				if (fieldVisible[i][j] == 0) {
-					System.out.print("?");
-				} else if (fieldVisible[i][j] == 50) {
-					System.out.print(" ");
-				} else {
-					System.out.print(fieldVisible[i][j]);
-				}
-				System.out.print(" | ");
-			}
-			System.out.print("\n");
-		}
-		
-		buildHidden(fieldHidden);
-	}
+    return sc.nextLine().replaceAll("\\s+", "");
+  }
 
 	
-	/**
-	 * Method to build hidden
-	 * 
-	 * @param fieldHidden
-	 */
-	public void buildHidden(int[][] fieldHidden) {
+  /**
+   * Method to set up the field for mine sweeper (visible)
+   */
+  public void displayMineField(int[][] fieldVisible, int[][] fieldHidden) {
 
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				int cnt = 0;
-				if (100 != fieldHidden[i][j]) {
-					if (0 != i) {
-						if (100 == fieldHidden[i - 1][j])
-							cnt++;
-						if (0 != j && 100 == fieldHidden[i - 1][j - 1]) {
-							cnt++;
-						}
+    System.out.print("\t ");
+    for (int i = 0; i < 10; i++) {
+      System.out.print(" " + i + "  ");
+    }
+    System.out.print("\n");
+    for (int i = 0; i < 10; i++) {
+      System.out.print(i + "\t| ");
+      for (int j = 0; j < 10; j++) {
+        if (fieldVisible[i][j] == 0) {
+          System.out.print("?");
+        } else if (fieldVisible[i][j] == 50) {
+          System.out.print(" ");
+        } else {
+          System.out.print(fieldVisible[i][j]);
+        }
+        System.out.print(" | ");
+      }
+      System.out.print("\n");
+    }
 
-					}
-					if (9 != i) {
-						if (100 == fieldHidden[i + 1][j])
-							cnt++;
-						if (9 != j && 100 == fieldHidden[i + 1][j + 1]) {
-							cnt++;
-						}
-					}
-					if (0 != j) {
-						if (100 == fieldHidden[i][j - 1])
-							cnt++;
-						if (9 != i && 100 == fieldHidden[i + 1][j - 1]) {
-							cnt++;
-						}
-					}
-					if (9 != j) {
-						if (100 == fieldHidden[i][j + 1])
-							cnt++;
-						if (0 != i && 100 == fieldHidden[i - 1][j + 1]) {
-							cnt++;
-						}
-					}
+    buildHidden(fieldHidden);
+  }
 
-					fieldHidden[i][j] = cnt;
-				}
-			}
-		}
-	}
+	
+  /**
+   * Method to build hidden
+   * 
+   * @param fieldHidden
+   */
+  public void buildHidden(int[][] fieldHidden) {
+
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        int cnt = 0;
+        if (100 != fieldHidden[i][j]) {
+          if (0 != i) {
+            if (100 == fieldHidden[i - 1][j])
+              cnt++;
+            if (0 != j && 100 == fieldHidden[i - 1][j - 1]) {
+              cnt++;
+            }
+
+          }
+          if (9 != i) {
+            if (100 == fieldHidden[i + 1][j])
+              cnt++;
+            if (9 != j && 100 == fieldHidden[i + 1][j + 1]) {
+              cnt++;
+            }
+          }
+          if (0 != j) {
+            if (100 == fieldHidden[i][j - 1])
+              cnt++;
+            if (9 != i && 100 == fieldHidden[i + 1][j - 1]) {
+              cnt++;
+            }
+          }
+          if (9 != j) {
+            if (100 == fieldHidden[i][j + 1])
+              cnt++;
+            if (0 != i && 100 == fieldHidden[i - 1][j + 1]) {
+              cnt++;
+            }
+          }
+
+          fieldHidden[i][j] = cnt;
+        }
+      }
+    }
+  }
 	
 	
 
-	/**
-	 * This method plants bomb randomly place bomb (a value of 100) in random x and y axis.
-	 * 
-	 * @param fieldHidden
-	 * @return
-	 */
-	public int[][] fieldSetup(int[][] fieldHidden) {
+  /**
+   * This method plants bomb randomly place bomb (a value of 100) in random x and y axis.
+   * 
+   * @param fieldHidden
+   * @return
+   */
+  public int[][] fieldSetup(int[][] fieldHidden) {
 
-		for (int count = 0; count < 10; count++) {
+    for (int count = 0; count < 10; count++) {
 
-			int x = ValueUtils.randomNumberGenerator(10);
+      int x = ValueUtils.randomNumberGenerator(10);
 
-			int y = ValueUtils.randomNumberGenerator(10);
+      int y = ValueUtils.randomNumberGenerator(10);
 
-			fieldHidden[x][y] = 100;
-		}
-		
-		return fieldHidden;
-	}
+      fieldHidden[x][y] = 100;
+    }
+
+    return fieldHidden;
+  }
 	
 	
-	/**
-	 * 
-	 * 
-	 * @return
-	 */
-	public boolean userMove(int[][] fieldVisible, int[][] fieldHidden) {
-//		Input Row & Col.
-		String[] xAndY = getUserInputInt("Enter X & Y Number : ").split(",");
-		int x = Integer.parseInt(xAndY[0]);
-		int y = Integer.parseInt(xAndY[1]);
+  /**
+   * 
+   * 
+   * @return
+   */
+  public boolean userMove(int[][] fieldVisible, int[][] fieldHidden) {
+    // Input Row & Col.
+    String[] xAndY = getUserInputInt("Enter X & Y Number : ").split(",");
+    int x = Integer.parseInt(xAndY[0]);
+    int y = Integer.parseInt(xAndY[1]);
 
-//		Check for invalid inputs
-		if (!ValueUtils.checkForInvalidValues(x, y, fieldVisible)) {
+    // Check for invalid inputs
+    if (!ValueUtils.checkForInvalidValues(x, y, fieldVisible)) {
 
-			if (100 == fieldHidden[x][y]) {
-				displayHidden(fieldHidden);
-				System.out.print("Oops! You stepped on a mine!\n============GAME OVER============");
-				return true;
-			} else if (0 == fieldHidden[x][y]) {
-				fixVisible(x, y, fieldVisible, fieldHidden);
-			} else {
-				fixNeighbours(x, y, fieldVisible, fieldHidden);
-			}
-			return false;
-		} else {
-			return true;
-		}
+      if (100 == fieldHidden[x][y]) {
+        displayHidden(fieldHidden);
+        System.out.print("Oops! You stepped on a mine!\n============GAME OVER============");
+        return true;
+      } else if (0 == fieldHidden[x][y]) {
+        fixVisible(x, y, fieldVisible, fieldHidden);
+      } else {
+        fixNeighbours(x, y, fieldVisible, fieldHidden);
+      }
+      return false;
+    } else {
+      System.out.println("Incorrect Input");
+      return true;
+    }
 
-	}
-
-
-	private void displayHidden(int[][] fieldHidden) {
-		System.out.print("\t ");
-		for (int i = 0; i < 10; i++) {
-			System.out.print(" " + i + "  ");
-		}
-		System.out.print("\n");
-		for (int i = 0; i < 10; i++) {
-			System.out.print(i + "\t| ");
-			for (int j = 0; j < 10; j++) {
-				if (fieldHidden[i][j] == 0) {
-					System.out.print(" ");
-				} else if (fieldHidden[i][j] == 100) {
-					System.out.print("X");
-				} else {
-					System.out.print(fieldHidden[i][j]);
-				}
-				System.out.print(" | ");
-			}
-			System.out.print("\n");
-		}
-	}
+  }
 
 
-	private void fixNeighbours(int x, int y, int[][] fieldVisible, int[][] fieldHidden) {
+  private void displayHidden(int[][] fieldHidden) {
+    System.out.print("\t ");
+    for (int i = 0; i < 10; i++) {
+      System.out.print(" " + i + "  ");
+    }
+    System.out.print("\n");
+    for (int i = 0; i < 10; i++) {
+      System.out.print(i + "\t| ");
+      for (int j = 0; j < 10; j++) {
+        if (fieldHidden[i][j] == 0) {
+          System.out.print(" ");
+        } else if (fieldHidden[i][j] == 100) {
+          System.out.print("X");
+        } else {
+          System.out.print(fieldHidden[i][j]);
+        }
+        System.out.print(" | ");
+      }
+      System.out.print("\n");
+    }
+  }
 
-		int randNum = ValueUtils.randomNumberBy4NoSize();
-		
-		fieldVisible[x][y] = fieldHidden[x][y];
 
-		if (0 == randNum) {
-			if (0 != x && 100 != fieldHidden[x - 1][y]) {
-					fieldVisible[x - 1][y] = fieldHidden[x - 1][y];
-					if (0 == fieldVisible[x - 1][y])
-						fieldVisible[x - 1][y] = 50;
-			}
-			if (0 != y && 100 != fieldHidden[x][y - 1]) {
+  private void fixNeighbours(int x, int y, int[][] fieldVisible, int[][] fieldHidden) {
 
-				fieldVisible[x][y - 1] = fieldHidden[x][y - 1];
-				if (0 == fieldVisible[x][y - 1])
-					fieldVisible[x][y - 1] = 50;
-			}
-			
-			if (0 != y && 100 != fieldHidden[x - 1][y - 1]) {
-				fieldVisible[x - 1][y - 1] = fieldHidden[x - 1][y - 1];
-				if (0 == fieldVisible[x - 1][y - 1])
-					fieldVisible[x - 1][y - 1] = 50;
-			}
-		} else if (randNum == 1) {
-			if (0 != x && 100 != fieldHidden[x - 1][y]) {
-					fieldVisible[x - 1][y] = fieldHidden[x - 1][y];
-					if (0 == fieldVisible[x - 1][y])
-						fieldVisible[x - 1][y] = 50;
-			}
-			if (9 != y && 100 != fieldHidden[x][y + 1]) {
-					fieldVisible[x][y + 1] = fieldHidden[x][y + 1];
-					if (0 == fieldVisible[x][y + 1])
-						fieldVisible[x][y + 1] = 50;
-			}
-			if (9 != y && x > 0 && 100 != fieldHidden[x - 1][y + 1]) {
-					fieldVisible[x - 1][y + 1] = fieldHidden[x - 1][y + 1];
-					if (fieldVisible[x - 1][y + 1] == 0)
-						fieldVisible[x - 1][y + 1] = 50;
-			}
-		} else if (2 == randNum) {
-			if (x != 9 && 100 != fieldHidden[x + 1][y]) {	
-					fieldVisible[x + 1][y] = fieldHidden[x + 1][y];
-					if (0 == fieldVisible[x + 1][y])
-						fieldVisible[x + 1][y] = 50;
-			}
-			if (y != 9 && 100 != fieldHidden[x][y + 1]) {
-					fieldVisible[x][y + 1] = fieldHidden[x][y + 1];
-					if (0 == fieldVisible[x][y + 1])
-						fieldVisible[x][y + 1] = 50;
-			}
-			if (x != 9 && y != 9 && 100 != fieldHidden[x + 1][y + 1]) {
-					fieldVisible[x + 1][y + 1] = fieldHidden[x + 1][y + 1];
-					if (0 == fieldVisible[x + 1][y + 1])
-						fieldVisible[x + 1][y + 1] = 50;
-			}
-		} else {
-			if (9 != x && 100 != fieldHidden[x + 1][y]) {
-					fieldVisible[x + 1][y] = fieldHidden[x + 1][y];
-					if (0 == fieldVisible[x + 1][y])
-						fieldVisible[x + 1][y] = 50;
-			}
-			if (0 != y && 100 != fieldHidden[x][y - 1]) {	
-					fieldVisible[x][y - 1] = fieldHidden[x][y - 1];
-					if (0 == fieldVisible[x][y - 1])
-						fieldVisible[x][y - 1] = 50;
-			}
-			if (9 != x && 0 != y && 100 != fieldHidden[x + 1][y - 1]) {
-				fieldVisible[x + 1][y - 1] = fieldHidden[x + 1][y - 1];
-				if (0 == fieldVisible[x + 1][y - 1])
-					fieldVisible[x + 1][y - 1] = 50;
-			}
-		}
+    int randNum = ValueUtils.randomNumberBy4NoSize();
 
-	}
+    fieldVisible[x][y] = fieldHidden[x][y];
 
-	private void fixVisible(int x, int y, int[][] fieldVisible, int[][] fieldHidden) {
-		fieldVisible[x][y] = 50;
-		if (0 != x) {
-			fieldVisible[x - 1][y] = fieldHidden[x - 1][y];
-			if (0 == fieldVisible[x - 1][y])
-				fieldVisible[x - 1][y] = 50;
-			if (0 != y) {
-				fieldVisible[x - 1][y - 1] = fieldHidden[x - 1][y - 1];
-				if (0 == fieldVisible[x - 1][y - 1])
-					fieldVisible[x - 1][y - 1] = 50;
-			}
-		}
-		if (9 != x) {
-			fieldVisible[x + 1][y] = fieldHidden[x + 1][y];
-			if (0 == fieldVisible[x + 1][y])
-				fieldVisible[x + 1][y] = 50;
-			if (9 != y) {
-				fieldVisible[x + 1][y + 1] = fieldHidden[x + 1][y + 1];
-				if (0 == fieldVisible[x + 1][y + 1])
-					fieldVisible[x + 1][y + 1] = 50;
-			}
-		}
-		if (0 != y) {
-			fieldVisible[x][y - 1] = fieldHidden[x][y - 1];
-			if (0 == fieldVisible[x][y - 1])
-				fieldVisible[x][y - 1] = 50;
-			if (9 != x) {
-				fieldVisible[x + 1][y - 1] = fieldHidden[x + 1][y - 1];
-				if (0 == fieldVisible[x + 1][y - 1])
-					fieldVisible[x + 1][y - 1] = 50;
-			}
-		}
-		if (9 != y) {
-			fieldVisible[x][y + 1] = fieldHidden[x][y + 1];
-			if (0 == fieldVisible[x][y + 1])
-				fieldVisible[x][y + 1] = 50;
-			if (0 != x) {
-				fieldVisible[x - 1][y + 1] = fieldHidden[x - 1][y + 1];
-				if (0 == fieldVisible[x - 1][y + 1])
-					fieldVisible[x - 1][y + 1] = 50;
-			}
-		}
+    if (0 == randNum) {
+      if (0 != x && 100 != fieldHidden[x - 1][y]) {
+        fieldVisible[x - 1][y] = fieldHidden[x - 1][y];
+        if (0 == fieldVisible[x - 1][y])
+          fieldVisible[x - 1][y] = 50;
+      }
+      if (0 != y && 100 != fieldHidden[x][y - 1]) {
 
-	}
+        fieldVisible[x][y - 1] = fieldHidden[x][y - 1];
+        if (0 == fieldVisible[x][y - 1])
+          fieldVisible[x][y - 1] = 50;
+      }
 
-	/**
-	 * Method to check if user have evaded all the mine on the play field.
-	 * 
-	 * @param fieldVisible
-	 * @param fieldHidden
-	 * @return
-	 */
-	public boolean chkWin(int[][] fieldVisible, int[][] fieldHidden) {
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (0 == fieldVisible[i][j] && 100 != fieldHidden[i][j]) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+      if (0 != y && 100 != fieldHidden[x - 1][y - 1]) {
+        fieldVisible[x - 1][y - 1] = fieldHidden[x - 1][y - 1];
+        if (0 == fieldVisible[x - 1][y - 1])
+          fieldVisible[x - 1][y - 1] = 50;
+      }
+    } else if (randNum == 1) {
+      if (0 != x && 100 != fieldHidden[x - 1][y]) {
+        fieldVisible[x - 1][y] = fieldHidden[x - 1][y];
+        if (0 == fieldVisible[x - 1][y])
+          fieldVisible[x - 1][y] = 50;
+      }
+      if (9 != y && 100 != fieldHidden[x][y + 1]) {
+        fieldVisible[x][y + 1] = fieldHidden[x][y + 1];
+        if (0 == fieldVisible[x][y + 1])
+          fieldVisible[x][y + 1] = 50;
+      }
+      if (9 != y && x > 0 && 100 != fieldHidden[x - 1][y + 1]) {
+        fieldVisible[x - 1][y + 1] = fieldHidden[x - 1][y + 1];
+        if (fieldVisible[x - 1][y + 1] == 0)
+          fieldVisible[x - 1][y + 1] = 50;
+      }
+    } else if (2 == randNum) {
+      if (x != 9 && 100 != fieldHidden[x + 1][y]) {
+        fieldVisible[x + 1][y] = fieldHidden[x + 1][y];
+        if (0 == fieldVisible[x + 1][y])
+          fieldVisible[x + 1][y] = 50;
+      }
+      if (y != 9 && 100 != fieldHidden[x][y + 1]) {
+        fieldVisible[x][y + 1] = fieldHidden[x][y + 1];
+        if (0 == fieldVisible[x][y + 1])
+          fieldVisible[x][y + 1] = 50;
+      }
+      if (x != 9 && y != 9 && 100 != fieldHidden[x + 1][y + 1]) {
+        fieldVisible[x + 1][y + 1] = fieldHidden[x + 1][y + 1];
+        if (0 == fieldVisible[x + 1][y + 1])
+          fieldVisible[x + 1][y + 1] = 50;
+      }
+    } else {
+      if (9 != x && 100 != fieldHidden[x + 1][y]) {
+        fieldVisible[x + 1][y] = fieldHidden[x + 1][y];
+        if (0 == fieldVisible[x + 1][y])
+          fieldVisible[x + 1][y] = 50;
+      }
+      if (0 != y && 100 != fieldHidden[x][y - 1]) {
+        fieldVisible[x][y - 1] = fieldHidden[x][y - 1];
+        if (0 == fieldVisible[x][y - 1])
+          fieldVisible[x][y - 1] = 50;
+      }
+      if (9 != x && 0 != y && 100 != fieldHidden[x + 1][y - 1]) {
+        fieldVisible[x + 1][y - 1] = fieldHidden[x + 1][y - 1];
+        if (0 == fieldVisible[x + 1][y - 1])
+          fieldVisible[x + 1][y - 1] = 50;
+      }
+    }
+
+  }
+
+  private void fixVisible(int x, int y, int[][] fieldVisible, int[][] fieldHidden) {
+    fieldVisible[x][y] = 50;
+    if (0 != x) {
+      fieldVisible[x - 1][y] = fieldHidden[x - 1][y];
+      if (0 == fieldVisible[x - 1][y])
+        fieldVisible[x - 1][y] = 50;
+      if (0 != y) {
+        fieldVisible[x - 1][y - 1] = fieldHidden[x - 1][y - 1];
+        if (0 == fieldVisible[x - 1][y - 1])
+          fieldVisible[x - 1][y - 1] = 50;
+      }
+    }
+    if (9 != x) {
+      fieldVisible[x + 1][y] = fieldHidden[x + 1][y];
+      if (0 == fieldVisible[x + 1][y])
+        fieldVisible[x + 1][y] = 50;
+      if (9 != y) {
+        fieldVisible[x + 1][y + 1] = fieldHidden[x + 1][y + 1];
+        if (0 == fieldVisible[x + 1][y + 1])
+          fieldVisible[x + 1][y + 1] = 50;
+      }
+    }
+    if (0 != y) {
+      fieldVisible[x][y - 1] = fieldHidden[x][y - 1];
+      if (0 == fieldVisible[x][y - 1])
+        fieldVisible[x][y - 1] = 50;
+      if (9 != x) {
+        fieldVisible[x + 1][y - 1] = fieldHidden[x + 1][y - 1];
+        if (0 == fieldVisible[x + 1][y - 1])
+          fieldVisible[x + 1][y - 1] = 50;
+      }
+    }
+    if (9 != y) {
+      fieldVisible[x][y + 1] = fieldHidden[x][y + 1];
+      if (0 == fieldVisible[x][y + 1])
+        fieldVisible[x][y + 1] = 50;
+      if (0 != x) {
+        fieldVisible[x - 1][y + 1] = fieldHidden[x - 1][y + 1];
+        if (0 == fieldVisible[x - 1][y + 1])
+          fieldVisible[x - 1][y + 1] = 50;
+      }
+    }
+
+  }
+
+  /**
+   * Method to check if user have evaded all the mine on the play field.
+   * 
+   * @param fieldVisible
+   * @param fieldHidden
+   * @return
+   */
+  public boolean chkWin(int[][] fieldVisible, int[][] fieldHidden) {
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        if (0 == fieldVisible[i][j] && 100 != fieldHidden[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 	
 	
 }
--- minesweeper/src/main/java/self/tut/game/utilities/ValueUtils.java
+++ minesweeper/src/main/java/self/tut/game/utilities/ValueUtils.java
@@ -3,56 +3,50 @@ package self.tut.game.utilities;
 import java.util.Random;
 
 public class ValueUtils {
-	
-	static Random random = new Random();
 
-	/**
-	 * Utility classes should not have public constructors (java:S1118)
-	 */
-	private ValueUtils() {
-		throw new IllegalStateException("Utility class");
-	}
-	
+  static Random random = new Random();
 
-	/**
-	 * This method return a number 
-	 * 
-	 * @param size
-	 * @return
-	 */
-	public static int randomNumberGenerator(int size) {
-		return random.nextInt(size);
-	}
-	
-	/**
-	 * 
-	 * 
-	 * @return
-	 */
-	public static int randomNumberBy4NoSize() {
-		return random.nextInt()%4;
-	}
-	
+  /**
+   * Utility classes should not have public constructors (java:S1118)
+   */
+  private ValueUtils() {
+    throw new IllegalStateException("Utility class");
+  }
 	
 
+  /**
+   * This method return a number
+   * 
+   * @param size
+   * @return
+   */
+  public static int randomNumberGenerator(int size) {
+    return random.nextInt(size);
+  }
 	
-	/**
-	 * This method takes in x and y along with field visible to validate for invalid values.
-	 * 
-	 * @param x
-	 * @param y
-	 * @param fieldVisible
-	 * @return
-	 */
-	public static boolean checkForInvalidValues(int x, int y, int[][] fieldVisible) {
+  /**
+   * 
+   * 
+   * @return
+   */
+  public static int randomNumberBy4NoSize() {
+    return random.nextInt() % 4;
+  }	
+	
 
-		if (x < 0 || x > 9 || y < 0 || y > 9 || 0 != fieldVisible[x][y]) {
-			System.out.println("Incorrect Input");
-			return true;
-		} else {
-			return false;
-		}
+	
+  /**
+   * This method takes in x and y along with field visible to validate for invalid values.
+   * 
+   * @param x
+   * @param y
+   * @param fieldVisible
+   * @return
+   */
+  public static boolean checkForInvalidValues(int x, int y, int[][] fieldVisible) {
 
-	}
+    return (x < 0 || x > 9 || y < 0 || y > 9 || 0 != fieldVisible[x][y]) ? true : false;
+
+  }
 
 }
